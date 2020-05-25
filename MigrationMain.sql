49fa71e49a0af95f0f4317d265b58fee22984e14
xyzMark <dieckman7461@gmail.com>
2020-05-25 19:14:29 -0400

    changed up the aggregation from the arch_doc subquery to the top level now that we know that the document change history can have multiple changes notices as well in what is still considered the current reivision. Note, added MIN() to the src.state. In the event the current revision is in both Document and arch_document it should default to 'active', 2154 rows

--- MigrationMain.sql
+++ MigrationMain.sql
@@ -1,5 +1,9 @@
-SELECT src.DOCUMENT_ID [Doc_Num], src.NAME [Doc_Name], dt.NAME [Doc_Type], src.state [Doc_Status], src.CREATE_DATE [Doc_Date], src.MODIFIED_DATE [Release_Date], src.obsDATE [ObseleteDate], 
-(UPPER((SUBSTRING(em.FIRST_NAME , 1, 1) +  em.LAST_NAME))) [Doc_Manager] , 'Yes' [ForceRevControl], SRC.REVISION_LEVEL [Revision], src.HISTORY [RevisionNotes]
+SELECT src.DOCUMENT_ID [Doc_Num], max(src.NAME) [Doc_Name], max(dt.NAME) [Doc_Type], min(src.state) [Doc_Status], max(src.CREATE_DATE) [Doc_Date], max(src.MODIFIED_DATE) [Release_Date], max(src.obsDATE) [ObseleteDate], 
+(UPPER((SUBSTRING(max(em.FIRST_NAME) , 1, 1) +  max(em.LAST_NAME)))) [Doc_Manager] , 'Yes' [ForceRevControl], SRC.REVISION_LEVEL [Revision],
+STUFF((SELECT ',' + hst.CHANGE_HISTORY
+		FROM IQSWeb.dbo.DOCM_CHNG_HIST hst
+		WHERE (hst.REVISION_LEVEL = src.REVISION_LEVEL AND hst.DOCUMENT_ID = src.DOCUMENT_ID)
+		FOR XML PATH ('')), 1, 1, '') [RevisionNotes]
 from (SELECT do.DOCUMENT_ID , do.NAME, do.REVISION_LEVEL, do.DOCUMENT_TYPE_ID, do.CREATE_DATE, do.MODIFIED_DATE, do.EMPLOYEE_ID, 
 		CASE do.REVISION_LEVEL 
 			when 'OBSOLETE' then 'OBSOLETE'
@@ -9,18 +13,14 @@ from (SELECT do.DOCUMENT_ID , do.NAME, do.REVISION_LEVEL, do.DOCUMENT_TYPE_ID, d
 		CASE do.REVISION_LEVEL 
 			when 'OBSOLETE' then do.REVISION_DATE 
 			else NULL 
-		END AS obsDATE,
-		'temp' HISTORY
+		END AS obsDATE
 	from IQSWeb.dbo.DOCUMENT do
 	union all
-	SELECT  MAX(da.DOCUMENT_ID) DOCUMENT_ID , MAX(da.NAME) NAME, da.REVISION_LEVEL REVISION_LEVEL , MAX(da.DOCUMENT_TYPE_ID) DOCUMENT_TYPE_ID , 
-		MAX(da.CREATE_DATE) CREATE_BY , MAX(da.MODIFIED_DATE) MODIFIED_DATE , MAX(da.EMPLOYEE_ID) EMPLOYEE_ID , 'OBSOLETE' state, MAX(da.ARCHIVE_DATE) obsDATE,
-		STUFF((SELECT ',' + hst.CHANGE_HISTORY 
-			FROM IQSWeb.dbo.DOCM_CHNG_HIST hst
-			WHERE (hst.REVISION_LEVEL = da.REVISION_LEVEL and MAX(da.DOCUMENT_ID ) = hst.DOCUMENT_ID )
-			FOR XML PATH ('')), 1, 1, '') HISTORY
-	FROM IQSWeb.dbo.DOCUMENT_ARCH da
-	GROUP BY da.REVISION_LEVEL, da.DOCUMENT_ID ) src
+	SELECT  da.DOCUMENT_ID DOCUMENT_ID , da.NAME NAME, da.REVISION_LEVEL REVISION_LEVEL , da.DOCUMENT_TYPE_ID DOCUMENT_TYPE_ID , 
+		da.CREATE_DATE CREATE_BY , da.MODIFIED_DATE MODIFIED_DATE , da.EMPLOYEE_ID EMPLOYEE_ID , 'OBSOLETE' state, da.ARCHIVE_DATE obsDATE
+	FROM IQSWeb.dbo.DOCUMENT_ARCH da) src
 left outer join IQSWeb.dbo.DOCUMENT_TYPE dt on src.DOCUMENT_TYPE_ID = dt.DOCUMENT_TYPE_ID 
 left outer join IQSWeb.dbo.EMPLOYEE em on src.EMPLOYEE_ID = em.EMPLOYEE_ID 
-order by  dt.NAME, src.DOCUMENT_ID, src.REVISION_LEVEL ASC 
+-- WHERE src.DOCUMENT_ID = 'wi-75-012'
+group by src.REVISION_LEVEL, src.DOCUMENT_ID
+order by  src.DOCUMENT_ID, src.REVISION_LEVEL ASC 
